770add472d068319fd71cfbbd8c1821f28772ae1
Pawel Banach <deto@op.pl>
2017-01-17 19:11:48 +0100

    Initial commit

---
+++ src/main/java/com/detoworks/DetoolsApplication.java
@@ -0,0 +1,12 @@
+package com.detoworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetoolsApplication.class, args);
+	}
+}
